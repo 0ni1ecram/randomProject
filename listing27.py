49d1212094c9bb33b0a850b2321a61583bc2c30f
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2022-09-07 16:28:00 +0400

    Current Time
    Simple python program that shows the current time at GMT

---
+++ listing27.py
@@ -0,0 +1,30 @@
+'''Show Current Time'''
+
+import time
+
+# Obtain current time
+t = time.time()
+
+# Total time in seconds (ts)
+ts = int(t)
+
+# Compute the current second (cs)
+cs = ts % 60
+
+# Compute Time in Minutes (tm)
+tm = ts // 60
+
+# Compute Current minutes (cm)
+cm = tm % 60
+
+# Compute time in Hours (tm)
+th = tm // 60
+
+# Current hour
+ch = th % 24
+
+
+print("Current time is", ch, ":", cm, ":", cs, "GMT")
+
+# UNIX epoch is the date and time the UNIX system dates from which is 1st Jan 1970
+
