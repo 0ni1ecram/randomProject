6194db2c2526f479057f4d1dae2a7f3f0f21929f
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2022-09-08 16:40:03 +0400

    Convert Pounds to Kilograms
    Simple program that coputes the conversion of mass in pounds to kilograms provided that 1 kilogram is equivalent to 0.454 pounds

---
+++ section24.py
@@ -0,0 +1,10 @@
+''' Convert Pounds to Kilograms '''
+
+# Prompt for user input
+pound = eval(input("Insert mass in pounds: "))
+
+# Commputation
+kilo = pound * 0.454
+
+# Display Results
+print(f'{pound} pounds is {kilo} kilograms')
