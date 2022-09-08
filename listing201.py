07f6a943c37e5d856d1f8d01f0f26088cf8547d0
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2022-09-08 15:59:31 +0400

    Convert Celcius to Farenheit
    Simple program that involves input temprature in degrees Celcius and the result will be revealed in degrees Farenheit

---
+++ listing201.py
@@ -0,0 +1,10 @@
+''' Convert Celcius to Farenheit '''
+
+# Input temprature in Celcius
+celcius = eval(input("Enter temprature in Celcius: "))
+
+# Calculate the temp in Farenheit
+farenheit = (9 / 5) * celcius + 32
+
+# Display result
+print(f'{celcius} Celcius is {farenheit} Farenheit')
