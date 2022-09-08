b09cefc61edaa53cce6de7b54f4c7026bc3488de
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2022-09-08 16:28:07 +0400

    Conversion of Feet to Meters
    Simple program that converts length in feet to length in meters.

---
+++ listing203.py
@@ -0,0 +1,10 @@
+''' Convert Feet to Meters '''
+
+# Prompt user to insert values in Feet
+feet = eval(input("Insert lenght in feet: "))
+
+# Compute to Meters
+meter = feet * 0.305
+
+# Display Result
+print(f'{feet} feet is {meter} meters')
