6e88452a61b4e1c9981a282285354281f3c80a67
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2022-09-10 12:02:46 +0400

    Area of Hexagon
    Simple program that computes the area of a hexagon provided the length of the side (identical sides)

---
+++ section215.py
@@ -0,0 +1,10 @@
+''' Area of a hexagon '''
+
+# Prompt user to insert side of a hexagon
+s = eval(input("Insert the side of the hexagon: "))
+
+# Compute the area of the hexagon
+area = ((3*(3**0.5)) / 2) * s**2
+
+# Display the area of hexagon
+print("The area of the hexagon is", area)
