23fe1537919e37166ea4c08a97e9a582cd54b887
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2022-09-20 13:23:57 +0400

    Math Functions at work
    Brief practise on how respective math functions operate.

---
+++ checkpoint31.py
@@ -0,0 +1,29 @@
+''' Evaluating Functions'''
+import math
+
+print("math.sqrt(4) =",math.sqrt(4))
+print("math.sin(2 * math.pi) =",math.sin(2 * math.pi))
+print("math.cos(2 * math.pi) =", math.cos(2 * math.pi))
+print("min(2,2,1) =",min(2,2,1))
+print("math.log(math.e) =",math.log(math.e))
+print("math.exp(1) =", math.exp(1))
+print("max(2 ,3 ,4) =", max(2,3,4))
+print("abs(-2.5) =", abs(-2.5))
+print("math.floor(-2.5) =", math.floor(-2.5))
+print("round(2.5) =", round(3.5))
+print("round(-2.5) =", round(-2.5))
+print("math.fabs(2.5) =", math.fabs(2.5))
+print("math.ceil(2.5) =", math.ceil(2.5))
+print("math.floor(2.5) =", math.floor(2.5))
+print("round(-2.5)", round(-2.5))
+print("round(2.6) =", round(2.6))
+print("round(math.fabs(2.5) =", round(math.fabs(-2.5)))
+
+# True. Angle in trigonometric function represents in radians
+
+# Conversion of 47deg to radians
+varA = math.radians(47)
+
+varB = math.degrees(math.pi / 7)
+
+print("\n math.degrees(47) =", varA, "\n math.radians(math.pi / 7) =", varB)
