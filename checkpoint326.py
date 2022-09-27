0b8488782d7133ff5a50828e4505626e3d6a3e53
Ndewawiyo <71815468+0ni1ecram@users.noreply.github.com>
2022-09-27 12:19:50 +0400

    Drawing Dot in Turtle.

---
+++ checkpoint326.py
@@ -0,0 +1,10 @@
+# Drawing a red Dot using turtle
+
+#Accepting user input
+x = eval(input("Insert the diameter of the dot: "))
+
+import turtle
+# turtle.dot(diameter, "color")
+turtle.dot(x, "red")
+
+turtle.done()
